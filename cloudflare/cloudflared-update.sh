a88e7fef20d95539131d39af617d4471bd57f138
Cole G <71441806+coleg06@users.noreply.github.com>
2022-10-20 21:02:07 -0400

    Added option to specify installed version

--- cloudflare/cloudflared-update.sh
+++ cloudflare/cloudflared-update.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # This script is intended to update Cloudflared on debian machines that had Cloudflared installed via Cloudflare's generated script on their Zero Trust page. 
 # I also haven't tested this on any other distro
+# Make sure to change version to what version fits your install. Options include: arm32, arm64, amd32, amd64.
+version="arm64"
 
-
-curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64.deb
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-$version.deb
 
 sudo dpkg -i cloudflared.deb 
 
